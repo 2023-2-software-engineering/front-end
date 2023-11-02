38a800f4b164f3ee852bdb39becae51e87f2b32c
ygreee <ygreee0320@gmail.com>
2023-11-03 01:19:20 +0900

    축제&게시판 페이지 생성

--- app/src/main/java/com/example/festival/ApiService.kt
+++ app/src/main/java/com/example/festival/ApiService.kt
@@ -2,8 +2,19 @@ package com.example.festival
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FestivalService {  // 페스티벌 리스트
     @GET("festival")
     fun getFestivalList(): Call<List<Festival>>
+}
+
+interface BoardListService {  // 게시판 리스트
+    @GET("partner")
+    fun getBoardList(): Call<List<Board>>
+}
+
+interface BoardDetailService {  // 게시판 상세
+    @GET("partner/{partnerId}")
+    fun getBoard(@Path("partnerId") boardId: Int): Call<Board>
 }
--- app/src/main/java/com/example/festival/DataModel.kt
+++ app/src/main/java/com/example/festival/DataModel.kt
@@ -9,3 +9,13 @@ data class Festival(
     val date: Date,
     val place: String
 )
+
+data class Board(
+    val partnerId: Int,
+    val title: String,
+    val content: String,
+    val createAt: Date,
+    val count: Int,
+    val nickname: String,
+    val address: String
+)
