7e1fa832d6c865622d19a61c96d412a3a9efbb59
jangjieun0675 <jangji0513@naver.com>
2023-11-08 14:49:25 +0900

    로그인/회원가입

--- app/src/main/java/com/example/festival/SignupActivity.kt
+++ app/src/main/java/com/example/festival/SignupActivity.kt
@@ -3,6 +3,7 @@ package com.example.festival
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.festival.JoinManager.sendJoinToServer
 import com.example.festival.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
@@ -19,8 +20,23 @@ class SignupActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.signupBtn.setOnClickListener {
+            val username = binding.name.text.toString()
+            val nickname = binding.nickname.text.toString()
+            val id = binding.id.text.toString()
+            val password = binding.password.text.toString()
+            val phone = binding.phoneNumber.text.toString()
+            val address = binding.address.text.toString()
+
+            val join = Join(username, nickname, id, address, password, phone)
+
+            sendJoinToServer(join)
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+
 
     }
-
-
 }
